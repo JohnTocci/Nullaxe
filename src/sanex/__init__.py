3034a19835bceaef8bae292d14951635473f06c3
JohnTocci <john@johnftocci.com>
2025-09-09 20:37:00 -0400

    Bump version to 0.3.0
    Update project version to 0.3.0 in pyproject.toml and __init__.py to reflect new release.

--- src/sanex/__init__.py
+++ src/sanex/__init__.py
@@ -2,7 +2,7 @@ from .cleaner import Sanex
 from .functions import *
 import sys
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 __author__ = "John Tocci"
 __email__ = "john@johntocci.com"
 __license__ = "MIT"
